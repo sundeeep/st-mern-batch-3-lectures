ff78a5507a1152dca4c19ef9ef9dbe5955919dcb
sundeeep <sundeeepdev@gmail.com>
2025-09-22 16:56:32 +0530

    functions are introduced to students

---
+++ basic-foundation/3_functions.js
@@ -0,0 +1,28 @@
+// console.log("Functions!!!")
+// Classic Function
+
+function getFullName(firstName) {
+    const lastName = "Dasari";
+    return `${firstName} ${lastName}`;
+}
+
+console.log(getFullName("Suresh"));
+let fullName = getFullName("Sundeeep");
+console.log(fullName)
+
+
+// const getFullName = () => {
+//     console.log("getFullName() is called!")
+// }
+// getFullName = 3;
+// getFullName();
+
+// ES modern arrow functions () => () , () => {}
+// let getFullName = (firstName) => {
+//     const lastName = "Dasari";
+//     return `${firstName} ${lastName}`;
+// }
+
+// console.log(getFullName("Suresh"));
+// let fullName = getFullName("Sundeeep");
+// console.log(fullName)

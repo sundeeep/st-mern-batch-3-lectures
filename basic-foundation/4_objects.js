827821ff9dd6cfcc38f3824a5fae272531a188c3
sundeeep <sundeeepdev@gmail.com>
2025-09-22 18:10:41 +0530

    combining objects and functions has been started!

---
+++ basic-foundation/4_objects.js
@@ -0,0 +1,31 @@
+// number, boolean, string
+console.log("Objects and Arrays Demo")
+//Step 1: create a bucket, database!
+const STUDENT_TRIBE = [];
+console.log(STUDENT_TRIBE);
+
+
+// Step 2: Create an individual Student object
+const sundeeep = {
+    name: "Sundeeep Dasari",
+    dob: "16-12-2000",
+    highSchool: "Sri Chaitanya Techno School",
+    hometown: "Diviseema",
+    noOfWorkExperiences: 3,
+    hasGraduated: true,
+}
+
+const keerthi = {
+    name: "Keerthi Dasari",
+    dob: "16-12-2002",
+    highSchool: "Sri Chaitanya Techno School",
+    hometown: "Diviseema",
+    noOfWorkExperiences: 0,
+    hasGraduated: true,
+}
+
+// Step 3: Push that data to the STUDENT_TRIBE database
+STUDENT_TRIBE.push(sundeeep);
+STUDENT_TRIBE.push(keerthi);
+
+console.log(STUDENT_TRIBE);
